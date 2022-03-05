54a6f7ac059e377e5066adb8b4404fb2f077cf39
mamun8893 <0316361936@student.wub.edu.bd>
2022-03-05 14:18:48 +0600

    feat: configure service & store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { userApi } from "../services/users";
+import logger from "redux-logger";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+setupListeners(store.dispatch);
